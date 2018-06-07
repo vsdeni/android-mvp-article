44eba4d43c29a51b6fd2ec0707e083915e1ff355
Denys Vasylenko <vs.deni@gmail.com>
2018-06-07 22:34:46 +0300

    update formatting

--- app/src/main/java/vsdeni/com/androidmvp/ProfileContract.kt
+++ app/src/main/java/vsdeni/com/androidmvp/ProfileContract.kt
@@ -1,28 +1,24 @@
 package vsdeni.com.androidmvp
 
-    interface ProfileView {
-        fun showName(name: String)
+interface ProfileView {
+    fun showName(name: String)
 
-        fun showCountry(country: Country?)
+    fun showCountry(country: Country?)
 
-        fun showCountriesPopup(countries: Collection<Country>)
+    fun showCountriesPopup(countries: Collection<Country>)
 
-        fun getName(): String
+    fun getName(): String
 
-        fun getCountry(): Country?
-    }
+    fun getCountry(): Country?
+}
 
-    interface ProfilePresenter {
-        fun onStart()
+interface ProfilePresenter {
+    fun onStart()
 
-        fun onStop()
+    fun onStop()
 
-        fun onCountryClick()
+    fun onCountryClick()
 
-        fun onSaveClick()
-    }
-
-
-
-
+    fun onSaveClick()
+}
 
