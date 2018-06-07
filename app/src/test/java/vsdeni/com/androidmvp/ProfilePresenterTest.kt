a7b1a12c717c21f8fcd02b90e2c4df9516750b1a
Denys Vasylenko <vs.deni@gmail.com>
2018-06-07 21:59:10 +0300

    add test case for ProfilePresenter

---
+++ app/src/test/java/vsdeni/com/androidmvp/ProfilePresenterTest.kt
@@ -0,0 +1,67 @@
+package vsdeni.com.androidmvp
+
+import io.reactivex.Observable
+import io.reactivex.android.plugins.RxAndroidPlugins
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnitRunner
+
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(MockitoJUnitRunner::class)
+class ProfilePresenterTest {
+
+    @Mock
+    private lateinit var profileView: ProfileView
+
+    @Mock
+    private lateinit var countriesRepository: CountriesRepository
+
+    @Mock
+    private lateinit var getProfileInteractor: GetProfileInteractor
+
+    @Mock
+    private lateinit var saveProfileInteractor: SaveProfileInteractor
+
+    private lateinit var profilePresenter: ProfilePresenter
+
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun setup() {
+            RxAndroidPlugins.setInitMainThreadSchedulerHandler {
+                io.reactivex.schedulers.Schedulers.trampoline()
+            }
+        }
+    }
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        `when`(countriesRepository.getCountries()).thenReturn(Observable.just(ABSENT_COUNTRY))
+
+        profilePresenter = ProfilePresenterImpl(profileView,
+                getProfileInteractor,
+                saveProfileInteractor,
+                GetCountriesInteractor(countriesRepository),
+                object : Schedulers {})
+    }
+
+    @Test
+    fun clickOnCountry_ShowCountriesPopup() {
+        profilePresenter.onCountryClick()
+        verify(profileView).showCountriesPopup(ArgumentMatchers.anyCollection())
+    }
+}
