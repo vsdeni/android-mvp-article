b22dae635e4328ee68b72699b8042d33e37bbb1a
Denys Vasylenko <vs.deni@gmail.com>
2018-06-17 00:07:52 +0300

    move activity with popup to a flavor

---
+++ app/src/popup/java/vsdeni/com/androidmvp/CountryPickerDialog.kt
@@ -0,0 +1,45 @@
+package vsdeni.com.androidmvp
+
+
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ListView
+
+class CountryPickerDialog : DialogFragment() {
+    companion object {
+        fun newInstance(title: String,
+                        items: Collection<Country>,
+                        country: Country): CountryPickerDialog {
+
+            val dialog = CountryPickerDialog()
+            val args = Bundle()
+            args.putString("title", title)
+            args.putParcelableArrayList("countries", ArrayList(items))
+            args.putParcelable("active", country)
+            dialog.arguments = args
+            return dialog
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: ListView = inflater.inflate(R.layout.dialog_countries, container, false) as ListView
+        val adapter = CountriesAdapter()
+        val items: ArrayList<Country> = arguments!!.getParcelableArrayList<Country>("countries")
+        val active: Country? = arguments?.getParcelable("active")
+        adapter.set(items, active)
+
+        view.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            if (activity is OnCountrySelectCallback) {
+                (activity as OnCountrySelectCallback).onCountrySelected(items[position])
+            }
+        }
+        view.adapter = adapter
+        return view
+    }
+}
+
+
---
+++ app/src/popup/java/vsdeni/com/androidmvp/ProfileActivity.kt
@@ -0,0 +1,58 @@
+package vsdeni.com.androidmvp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.popup.activity_profile.*
+
+class ProfileActivity : AppCompatActivity(), ProfileView, OnCountrySelectCallback {
+    private lateinit var presenter: ProfilePresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = getString(R.string.toolbar_title_profile)
+
+        presenter = Injector(this).provideProfilePresenter()
+        presenter.attachView(this)
+
+        user_country.setOnClickListener({ presenter.loadCountries() })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.loadProfile()
+    }
+
+    override fun onCountrySelected(country: Country) {
+
+    }
+
+    override fun showName(name: String) {
+        user_name.setText(name)
+    }
+
+    override fun showCountry(country: Country?) {
+        user_country.text = country?.name
+        user_country.tag = country
+    }
+
+    override fun showCountriesPicker(countries: Collection<Country>) {
+        val dialog = CountryPickerDialog.newInstance("Countries", countries, country())
+        dialog.show(supportFragmentManager, "countries")
+    }
+
+    override fun name(): String =
+            user_name.text.toString()
+
+
+    override fun country(): Country =
+            user_country.tag as Country
+
+}
+
+interface OnCountrySelectCallback {
+    fun onCountrySelected(country: Country)
+}
